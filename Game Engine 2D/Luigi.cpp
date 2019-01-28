edca6bcd0923102eaf88699cef04917aebedfaea
Andrea Catalini <andrea.catalini89@gmail.com>
2019-01-28 17:08:01 +0100

    Adjustment on Player and Luigi for the changes in the animation department.

--- Game Engine 2D/Luigi.cpp
+++ Game Engine 2D/Luigi.cpp
@@ -10,15 +10,16 @@
 #include "Transform.hpp"
 #include "Patrolling.hpp"
 #include "Repeater.hpp"
+#include "FieldOfView.hpp"
 
 void Luigi::Init()
 {
 	World& world = GetWorld();
 	const unsigned long int entityIndex = GetEntityIndex();
-	EntityManager::AddComponent(world, entityIndex, new Appearance(world, entityIndex, Constants::RESOURCE_PATH + Constants::PLAYER_SPRITE_PATH));
-	Appearance* appearance = static_cast<Appearance*>(world.EntitiesComponentsMatrix[entityIndex][Appearance::Id]);
+
 	sf::IntRect spriteRect(498, 12, Constants::PLAYER_WIDTH, Constants::PLAYER_HEIGHT);
-	appearance->GetSprite()->setTextureRect(spriteRect);
+	EntityManager::AddComponent(world, entityIndex, new Appearance(world, entityIndex, Constants::RESOURCE_PATH + Constants::PLAYER_SPRITE_PATH, spriteRect));
+
 	EntityManager::AddComponent(world, entityIndex, new Transform(world, entityIndex));
 	Transform* trans = static_cast<Transform*>(world.EntitiesComponentsMatrix[entityIndex][Transform::Id]);
 	trans->SetPosition(sf::Vector2f(Constants::PLAYER_PHYSICAL_STARTING_X + 30, Constants::PLAYER_PHYSICAL_STARTING_Y + 87));
@@ -26,11 +27,12 @@ void Luigi::Init()
 	EntityManager::AddComponent(world, entityIndex, new Acceleration(world, entityIndex, Constants::PLAYER_MAX_ACCELERATION_X, Constants::PLAYER_MAX_ACCELERATION_Y));
 	EntityManager::AddComponent(world, entityIndex, new Controller(world, entityIndex));
 	EntityManager::AddComponent(world, entityIndex, new Velocity(world, entityIndex));
+	EntityManager::AddComponent(world, entityIndex, new FieldOfView(world, entityIndex, 100.f, 15.f, sf::Vector2f(1,0)));
 
-	BT::BehaviourTree* bt = new BT::BehaviourTree(world, entityIndex, *this);
+	/*BT::BehaviourTree* bt = new BT::BehaviourTree(world, entityIndex, *this);
 	std::unique_ptr<BT::Repeater> rootRepeater = std::make_unique<BT::Repeater>(nullptr, std::vector<std::unique_ptr<BT::Node>>(), *bt);
 	std::unique_ptr<BT::Patrolling> patrNode = std::make_unique<BT::Patrolling>(rootRepeater.get(), std::vector<std::unique_ptr<BT::Node>>(), *bt);
 	rootRepeater->AddChild(std::move(patrNode));
 	bt->SetRoot(std::move(rootRepeater));
-	EntityManager::AddComponent(world, entityIndex, bt);
+	EntityManager::AddComponent(world, entityIndex, bt);*/
 }
--- Game Engine 2D/Player.cpp
+++ Game Engine 2D/Player.cpp
@@ -21,17 +21,21 @@
 
 //Events
 #include "InputEvent.hpp"
-
 #include "Animations.hpp"
 
 void Player::Init()
 {
 	World& world = GetWorld();
 	const unsigned long int entityIndex = GetEntityIndex();
-	EntityManager::AddComponent(world, entityIndex, new Appearance(world, entityIndex, Constants::RESOURCE_PATH + Constants::PLAYER_SPRITE_PATH));
+
+	std::string playerSpritePath = Constants::RESOURCE_PATH + Constants::PLAYER_SPRITE_PATH;
+	sf::IntRect spriteRect(498, 12, Constants::PLAYER_WIDTH, Constants::PLAYER_HEIGHT);
+	EntityManager::AddComponent(world, entityIndex, new Appearance(world, entityIndex, playerSpritePath, spriteRect));
+
 	EntityManager::AddComponent(world, entityIndex, new Transform(world, entityIndex));
-    (static_cast<Transform*>(world.EntitiesComponentsMatrix[entityIndex][Transform::Id]))->SetPosition(sf::Vector2f(Constants::PLAYER_PHYSICAL_STARTING_X, Constants::PLAYER_PHYSICAL_STARTING_Y));
-    
+    //(static_cast<Transform*>(world.EntitiesComponentsMatrix[entityIndex][Transform::Id]))->SetPosition(sf::Vector2f(Constants::PLAYER_PHYSICAL_STARTING_X, Constants::PLAYER_PHYSICAL_STARTING_Y));
+    GetComponent<Transform>()->SetPosition(sf::Vector2f(Constants::PLAYER_PHYSICAL_STARTING_X, Constants::PLAYER_PHYSICAL_STARTING_Y));
+
     EntityManager::AddComponent(world, entityIndex, new Acceleration(world, entityIndex, Constants::PLAYER_MAX_ACCELERATION_X, Constants::PLAYER_MAX_ACCELERATION_Y));
 	Controller* controllerComp = new Controller(world, entityIndex);
     EntityManager::AddComponent(world, entityIndex, controllerComp);
@@ -39,25 +43,27 @@ void Player::Init()
     EntityManager::AddComponent(world, entityIndex, new Collider(world, entityIndex, sf::Vector2f(0.5f, 0.5f)));
     
 
-	//Animation states
+	//Animation states - start
+	const auto* const playerSprite = GetComponent<Appearance>()->GetSprite();
     std::map<AnimationState, AnimationData*> animationMap;
-    AnimationData* walkingAnim = new AnimationData(sf::Vector2f(Constants::PLAYER_SPRITE_STARTING_X, Constants::PLAYER_SPRITE_STARTING_Y),
+    AnimationData* walkingAnim = new AnimationData(playerSprite, sf::Vector2f(Constants::PLAYER_SPRITE_STARTING_X, Constants::PLAYER_SPRITE_STARTING_Y),
                            sf::Vector2f(Constants::SPACE_BETWEEN_SPRITE_X, Constants::SPACE_BETWEEN_SPRITE_Y), false,
                            false, Constants::PLAYER_WIDTH, Constants::PLAYER_HEIGHT, Constants::PLAYER_SPRITE_MAX_FRAME, Constants::ANIMATION_FRAMERATE);
     
     animationMap.insert( std::pair<AnimationState, AnimationData*>(AnimationState::WALKING, walkingAnim) );
 
-	AnimationData* idleAnim = new AnimationData(sf::Vector2f(14, 10),
+	AnimationData* idleAnim = new AnimationData(playerSprite, sf::Vector2f(14, 10),
 		sf::Vector2f(Constants::SPACE_BETWEEN_SPRITE_X, Constants::SPACE_BETWEEN_SPRITE_Y), false,
 		false, Constants::PLAYER_WIDTH, Constants::PLAYER_HEIGHT, 1, Constants::ANIMATION_FRAMERATE);
 
 	animationMap.insert(std::pair<AnimationState, AnimationData*>(AnimationState::IDLE, idleAnim));
 
-	AnimationData* jumpAnim = new AnimationData(sf::Vector2f(115, 7),
+	AnimationData* jumpAnim = new AnimationData(playerSprite, sf::Vector2f(115, 7),
 		sf::Vector2f(Constants::SPACE_BETWEEN_SPRITE_X, Constants::SPACE_BETWEEN_SPRITE_Y), false,
 		false, 16, Constants::PLAYER_HEIGHT, 1, Constants::ANIMATION_FRAMERATE);
 
 	animationMap.insert(std::pair<AnimationState, AnimationData*>(AnimationState::JUMPING, jumpAnim));
+	//Animation states - end
 
 	Animation* animationComp = new Animation(world, entityIndex, AnimationState::IDLE, animationMap, true);
     EntityManager::AddComponent(world, entityIndex, animationComp);
