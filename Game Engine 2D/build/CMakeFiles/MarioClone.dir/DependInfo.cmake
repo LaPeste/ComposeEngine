bad98dbf0d3ed1b06662b2ccf3b9d9a0c82eda3e
Andrea Catalini <andrea.catalini89@gmail.com>
2019-09-01 12:20:11 +0200

    Build folder was added as the CMake script isn't perfect yet

---
+++ Game Engine 2D/build/CMakeFiles/MarioClone.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/andrea/Development/ComposeEngineCMake/src/AI/BasicNodes/Repeater.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/AI/BasicNodes/Repeater.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/AI/BasicNodes/Selector.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/AI/BasicNodes/Selector.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/AI/BasicNodes/Sequence.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/AI/BasicNodes/Sequence.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/AI/BehaviourTree.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/AI/BehaviourTree.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Animations/Animation.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Animations/Animation.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Animations/Animations.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Animations/Animations.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Camera.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Camera.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/ClassRegistry.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/ClassRegistry.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Collisions/CollisionDetectionUtils.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Collisions/CollisionDetectionUtils.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Collisions/CollisionEvent.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Collisions/CollisionEvent.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Components/Acceleration.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Components/Acceleration.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Components/Appearance.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Components/Appearance.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Components/Collider.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Components/Collider.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Components/ComponentBase.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Components/ComponentBase.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Components/Controller.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Components/Controller.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Components/EntityFlag.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Components/EntityFlag.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Components/Transform.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Components/Transform.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Components/Velocity.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Components/Velocity.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Constants.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Constants.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/DebugUtils/GeometryDebugger.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/DebugUtils/GeometryDebugger.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Engine.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Engine.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Entities/EntityManager.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Entities/EntityManager.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Entities/EntityUtils.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Entities/EntityUtils.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Events/AddedComponentEvent.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Events/AddedComponentEvent.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Events/EventBase.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Events/EventBase.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Events/EventListener.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Events/EventListener.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Events/EventManager.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Events/EventManager.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Events/InputEvent.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Events/InputEvent.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/FPS.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/FPS.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/GameExample/AI/AttackWhenInRange.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/GameExample/AI/AttackWhenInRange.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/GameExample/AI/Patrolling.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/GameExample/AI/Patrolling.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/GameExample/Behaviours/Components/FieldOfView.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/GameExample/Behaviours/Components/FieldOfView.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/GameExample/Behaviours/Systems/FOVspotter.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/GameExample/Behaviours/Systems/FOVspotter.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/GameExample/GameObjects/Door.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/GameExample/GameObjects/Door.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/GameExample/GameObjects/Luigi.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/GameExample/GameObjects/Luigi.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/GameObjects/GameObject.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/GameObjects/GameObject.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/GameObjects/Player.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/GameObjects/Player.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/SfmlInputEvent.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/SfmlInputEvent.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Systems/Animator.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Systems/Animator.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Systems/BTEngine.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Systems/BTEngine.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Systems/CollisionDetector.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Systems/CollisionDetector.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Systems/InputSystem.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Systems/InputSystem.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Systems/Movement.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Systems/Movement.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Systems/Renderer.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Systems/Renderer.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Systems/SystemBase.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Systems/SystemBase.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Systems/SystemManager.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Systems/SystemManager.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Utils/UtilConstants.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Utils/UtilConstants.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/Utils/Utils.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/Utils/Utils.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/World.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/World.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/main.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/main.cpp.o"
+  "/home/andrea/Development/ComposeEngineCMake/src/stdafx.cpp" "/home/andrea/Development/ComposeEngineCMake/build/CMakeFiles/MarioClone.dir/src/stdafx.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../include"
+  "../lib"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Game Engine 2D/build/cmake_install.cmake
@@ -0,0 +1,73 @@
+# Install script for directory: /home/andrea/Development/ComposeEngineCMake
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/GameTest/MarioClone" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/GameTest/MarioClone")
+    file(RPATH_CHECK
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/GameTest/MarioClone"
+         RPATH "")
+  endif()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/GameTest" TYPE EXECUTABLE FILES "/home/andrea/Development/ComposeEngineCMake/build/MarioClone")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/GameTest/MarioClone" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/GameTest/MarioClone")
+    file(RPATH_CHANGE
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/GameTest/MarioClone"
+         OLD_RPATH "/usr/local/lib:"
+         NEW_RPATH "")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/GameTest/MarioClone")
+    endif()
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE FILE FILES "/home/andrea/Development/ComposeEngineCMake/build/EngineConfig.h")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "/home/andrea/Development/ComposeEngineCMake/build/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
