4d06fa83f43cd51060e432c06b927b8cea56be2e
Andrea Catalini <ac_workrelated@tutanota.com>
2023-08-22 16:43:32 +0200

    More improvements on main

--- GameEngine/src/main.cpp
+++ GameEngine/src/main.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char* argv[])
 {
-	std::ostringstream oss;
+	std::ostringstream oss {};
 	oss << "Engine version " << ENGINE_VERSION_MAJOR << "." << ENGINE_VERSION_MINOR;
 	DEBUG_LOG(oss.str());
 
@@ -17,24 +17,31 @@ int main(int argc, char* argv[])
 		Engine& e = Engine::GetInstance();
 		e.Launch(&window);
 	}
-	catch (std::bad_alloc)
+	catch (const std::bad_alloc &)
 	{
 		DEBUG_ERROR("it was not possible to instantiate a new object. The program is being termianted!");
 		return EXIT_FAILURE;
 	}
-	catch (std::out_of_range)
+	catch (const std::out_of_range &)
 	{
 		DEBUG_ERROR("Silly man, you were trying to access something out of the memory range of your container! You have crashed the application, well done!");
 		return EXIT_FAILURE;
 	}
 	catch (const int& e)
 	{
-		std::ostringstream oss;
+		std::ostringstream oss {};
 		oss << "exception" << e << " was raised. The program is being termianted!";
 		DEBUG_ERROR(oss.str());
 		return EXIT_FAILURE;
 	}
 #ifndef INTERNAL_DEBUG
+	catch (const std::exception & ex)
+	{
+		std::ostringstream oss {};
+		oss << "An exception of undetermined type was raised: " << ex.what();
+		DEBUG_ERROR(oss.str());
+		return EXIT_FAILURE;
+	}
 	catch (...)
 	{
 		DEBUG_ERROR("An exception of undetermined type was raised");
