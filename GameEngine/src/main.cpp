6e14911e74a8d1442894ceb8adc815a18ec4087e
Andrea Catalini <ac_workrelated@tutanota.com>
2023-08-22 16:38:01 +0200

    Main gets some clean-up

--- GameEngine/src/main.cpp
+++ GameEngine/src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
 {
 	std::ostringstream oss;
 	oss << "Engine version " << ENGINE_VERSION_MAJOR << "." << ENGINE_VERSION_MINOR;
-	Utils::PrintDebugLog(_FUNCTION_NAME_, oss.str());
+	DEBUG_LOG(oss.str());
 
 	//try to catch exceptions in order to allow stack unwinding
 	try
@@ -16,32 +16,31 @@ int main(int argc, char* argv[])
 		sf::RenderWindow window;
 		Engine& e = Engine::GetInstance();
 		e.Launch(&window);
-#ifdef LOG_OUTPUT_CONSOLE
-		std::string methodName = _FUNCTION_NAME_;
-		Utils::PrintDebugLog(methodName, "terminated");
-#endif
 	}
 	catch (std::bad_alloc)
 	{
-		std::ostringstream oss;
-		oss << "it was not possible to instantiate a new object. The program is being termianted!";
-		Utils::PrintDebugError(_FUNCTION_NAME_, oss.str());
+		DEBUG_ERROR("it was not possible to instantiate a new object. The program is being termianted!");
 		return EXIT_FAILURE;
 	}
 	catch (std::out_of_range)
 	{
-		std::ostringstream oss;
-		oss << "Silly man, you were trying to access something out of the memory range of your container! You have crashed the application, well done!";
-		Utils::PrintDebugError(_FUNCTION_NAME_, oss.str());
+		DEBUG_ERROR("Silly man, you were trying to access something out of the memory range of your container! You have crashed the application, well done!");
 		return EXIT_FAILURE;
 	}
 	catch (const int& e)
 	{
 		std::ostringstream oss;
 		oss << "exception" << e << " was raised. The program is being termianted!";
-		Utils::PrintDebugError(_FUNCTION_NAME_, oss.str());
+		DEBUG_ERROR(oss.str());
 		return EXIT_FAILURE;
 	}
+#ifndef INTERNAL_DEBUG
+	catch (...)
+	{
+		DEBUG_ERROR("An exception of undetermined type was raised");
+		return EXIT_FAILURE;
+	}
+#endif
 
 	return EXIT_SUCCESS;
 }
